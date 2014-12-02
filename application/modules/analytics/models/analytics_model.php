458eb7aa94a8e0acc50ef2427034605b69fc0ad8
unknown <Rufus.Mbugua@VDI-HP-LAB-4.strathmore.local>
2014-12-02 15:52:51 +0000

    fixed map

--- application/modules/analytics/models/analytics_model.php
+++ application/modules/analytics/models/analytics_model.php
@@ -2824,7 +2824,7 @@ ORDER BY f.fac_county ASC;";
          */
         public function runMap($survey, $survey_category, $statistic) {
             $myData = array();
-            $counties = $this->getAllCountyNames();
+            $counties = $this->getCounties();
             foreach ($counties as $county) {
                 $countyName = $county['countyName'];
                 
